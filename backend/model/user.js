9b31a961cbe82f1d08738d1c17c11c293cba32c2
chihuahua011 <monoki07@gmail.com>
2021-09-13 11:39:46 +0200

    be heroku deploy

--- backend/model/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const user = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
-}, {collection: "users"});
-
-module.exports = mongoose.model("User", user);
