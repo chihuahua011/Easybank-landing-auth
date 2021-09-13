d0ed6dad56c54cb2e7e9d001af92faab549ed749
chihuahua011 <monoki07@gmail.com>
2021-09-13 12:20:18 +0200

    503 error handling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,72 +6,76 @@ import "./Login.css";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import decode from "jwt-decode"
+import decode from "jwt-decode";
 
 const Login = () => {
-  const [logEmail, setLogEmail] = useState("");
-  const [logPass, setLogPass] = useState("");
-  const history = useHistory();
+    const [logEmail, setLogEmail] = useState("");
+    const [logPass, setLogPass] = useState("");
+    const history = useHistory();
 
-  const redirect = () => {
-    history.push("/");
-    window.location.reload();
-  };
+    const redirect = () => {
+        history.push("/");
+        window.location.reload();
+    };
 
-  const logSubmit = () => {
-    axios
-      .post("https://easybank-be.herokuapp.com/api/login", {
-        email: logEmail,
-        password: logPass,
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          localStorage.setItem("token", res.data.token);
-          setTimeout(redirect, 500);
-          console.log(res.data);
-          console.log(decode(res.data.token))
-        }
-      })
-      .catch((err) => {
-        if (err.response.data.msg === "Missing input")
-          toast.error("Please fill out all fields!", {
-            position: toast.POSITION.BOTTOM_CENTER,
-          });
-        if (err.response.data.msg === "invalid user")
-          toast.error("Invalid credentials!", {
-            position: toast.POSITION.BOTTOM_CENTER,
-          });
-      });
-  };
+    const logSubmit = () => {
+        axios
+            .post("https://easybank-be.herokuapp.com/api/login", {
+                email: logEmail,
+                password: logPass,
+            })
+            .then((res) => {
+                if (res.status === 200) {
+                    localStorage.setItem("token", res.data.token);
+                    setTimeout(redirect, 500);
+                    console.log(res.data);
+                    console.log(decode(res.data.token));
+                }
+            })
+            .catch((err) => {
+                if (err.request)
+                    toast.error("Server is down right now! Check back later!", {
+                        position: toast.POSITION.BOTTOM_CENTER,
+                    });
+                else if (err.response.data.msg === "Missing input")
+                    toast.error("Please fill out all fields!", {
+                        position: toast.POSITION.BOTTOM_CENTER,
+                    });
+                else if (err.response.data.msg === "invalid user")
+                    toast.error("Invalid credentials!", {
+                        position: toast.POSITION.BOTTOM_CENTER,
+                    });
+            });
+    };
 
-  return (
-    <section id="logSection">
-      <div id="regCont">
-        <p className="regP">Login to your account</p>
-        <input
-          className="input"
-          type="email"
-          placeholder="Email"
-          required
-          onChange={(e) => setLogEmail(e.target.value)}
-        />
-        <input
-          className="input"
-          type="password"
-          placeholder="Password"
-          required
-          onChange={(e) => setLogPass(e.target.value)}
-        />
-        <button className="logSubmit" onClick={logSubmit}>
-          Login
-        </button>
-      </div>
-      <img className="mockups" src={Mockups} alt="" />
-      <img className="bgPlanes" src={BgPlanes} alt="" />
-      <div className="regBg"></div>
-      <ToastContainer />
-    </section>
-  );
+    return (
+        <section id="logSection">
+            <div id="regCont">
+                <p className="regP">Login to your account</p>
+                <input
+                    className="input"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    onChange={(e) => setLogEmail(e.target.value)}
+                />
+                <input
+                    className="input"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    onChange={(e) => setLogPass(e.target.value)}
+                />
+                <button className="logSubmit" onClick={logSubmit}>
+                    Login
+                </button>
+            </div>
+            <img className="mockups" src={Mockups} alt="" />
+            <img className="bgPlanes" src={BgPlanes} alt="" />
+            <div className="regBg"></div>
+            <ToastContainer />
+        </section>
+    );
 };
 
 export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,93 +1,100 @@
 import React, { useState } from "react";
 import "./Register.css";
-import { useHistory } from 'react-router';
+import { useHistory } from "react-router";
 import BgPlanes from "../../images/bg-intro-desktop.svg";
 import Mockups from "../../images/image-mockups.png";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
-  const [regName, setRegName] = useState("");
-  const [regEmail, setRegEmail] = useState("");
-  const [regPass, setRegPass] = useState("");
-  const history = useHistory();
+    const [regName, setRegName] = useState("");
+    const [regEmail, setRegEmail] = useState("");
+    const [regPass, setRegPass] = useState("");
+    const history = useHistory();
 
-  const redirect = () => {
-    return history.push("/login")
-  }
+    const redirect = () => {
+        return history.push("/login");
+    };
 
-  const regSubmit = () => {
-    axios
-      .post("https://easybank-be.herokuapp.com/api/register", {
-        name: regName,
-        email: regEmail,
-        password: regPass,
-      })
-      .then((res) => {
-        if (res.status === 201) {
-          console.log("Registration success")
-          setTimeout(redirect, 500)
-        }
-      })
-      .catch((err) => {
-        if (err.response.data.msg === "Already registered email")
-          toast.error("Already registered user!", {
-            position: toast.POSITION.BOTTOM_CENTER,
-          });
-        if (err.response.data.msg === "Password too short")
-          toast.error("Password should be at least 6 characters long!", {
-            position: toast.POSITION.BOTTOM_CENTER,
-          });
-        if (err.response.data.msg === "Fill out all inputs")
-            toast.error("Please fill out all fields!", {
-            position: toast.POSITION.BOTTOM_CENTER,
-          });
-        if (err.response.status === 500)
-          toast.error("Data could not be processed!", {
-            position: toast.POSITION.BOTTOM_CENTER,
-          });
-      });
-  };
+    const regSubmit = () => {
+        axios
+            .post("https://easybank-be.herokuapp.com/api/register", {
+                name: regName,
+                email: regEmail,
+                password: regPass,
+            })
+            .then((res) => {
+                if (res.status === 201) {
+                    console.log("Registration success");
+                    setTimeout(redirect, 500);
+                }
+            })
+            .catch((err) => {
+                if (err.request)
+                    toast.error("Server is down right now! Check back later!", {
+                        position: toast.POSITION.BOTTOM_CENTER,
+                    });
+                else if (err.response.data.msg === "Already registered email")
+                    toast.error("Already registered user!", {
+                        position: toast.POSITION.BOTTOM_CENTER,
+                    });
+                else if (err.response.data.msg === "Password too short")
+                    toast.error(
+                        "Password should be at least 6 characters long!",
+                        {
+                            position: toast.POSITION.BOTTOM_CENTER,
+                        }
+                    );
+                else if (err.response.data.msg === "Fill out all inputs")
+                    toast.error("Please fill out all fields!", {
+                        position: toast.POSITION.BOTTOM_CENTER,
+                    });
+                else if (err.response.status === 500)
+                    toast.error("Data could not be processed!", {
+                        position: toast.POSITION.BOTTOM_CENTER,
+                    });
+            });
+    };
 
-  return (
-    <>
-      <section id="regSection">
-        <div id="regCont">
-            <p className="regP" >Registration form</p>
-          <input
-            required
-            className="input"
-            type="text"
-            placeholder="Name"
-            onChange={(e) => setRegName(e.target.value)}
-          />
-          <input
-            required
-            className="input"
-            type="email"
-            placeholder="Email"
-            onChange={(e) => setRegEmail(e.target.value)}
-          />
-          <input
-            required
-            className="input"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setRegPass(e.target.value)}
-          />
-          <button className="regSubmit" onClick={regSubmit}>
-            Submit
-          </button>
-          <p className="regPBottom" >All fields are required!</p>
-        </div>
-        <img className="mockups" src={Mockups} alt="" />
-        <img className="bgPlanes" src={BgPlanes} alt="" />
-        <div className="regBg"></div>
-        <ToastContainer />
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section id="regSection">
+                <div id="regCont">
+                    <p className="regP">Registration form</p>
+                    <input
+                        required
+                        className="input"
+                        type="text"
+                        placeholder="Name"
+                        onChange={(e) => setRegName(e.target.value)}
+                    />
+                    <input
+                        required
+                        className="input"
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e) => setRegEmail(e.target.value)}
+                    />
+                    <input
+                        required
+                        className="input"
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setRegPass(e.target.value)}
+                    />
+                    <button className="regSubmit" onClick={regSubmit}>
+                        Submit
+                    </button>
+                    <p className="regPBottom">All fields are required!</p>
+                </div>
+                <img className="mockups" src={Mockups} alt="" />
+                <img className="bgPlanes" src={BgPlanes} alt="" />
+                <div className="regBg"></div>
+                <ToastContainer />
+            </section>
+        </>
+    );
 };
 
 export default Register;
